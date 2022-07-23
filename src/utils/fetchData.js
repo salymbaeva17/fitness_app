c42666bbab08287aa6acf57ec85251b13d476b7f
salymbaeva17 <salymbaeva17@gmail.com>
2022-07-23 23:58:43 +0600

    !

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': `2ccd011c6dmshea15a727a7a49cdp1c9a47jsn6b1b6e824769`,
+        'X-RapidAPI-Key': '2ccd011c6dmshea15a727a7a49cdp1c9a47jsn6b1b6e824769',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
